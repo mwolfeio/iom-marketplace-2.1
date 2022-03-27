9310b7c7b7109ab06cb7f94be24f5bb0647cf9cc
Matt <90003090+mwolfeio@users.noreply.github.com>
2022-03-27 11:43:16 -0700

    fixed offers page

---
+++ pages/wallet/offers.tsx
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import React, { useState } from "react";
+import Router from "next/router";
+
+import Layout from "comps/Layout";
+import Deposit from "comps/Deposit";
+import History from "comps/DepositHistory";
+import BreadCrumbs from "comps/BreadCrumbs";
+
+export default function Comp() {
+  return <Layout>Active Offers</Layout>;
+}
