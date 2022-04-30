68aaf189166c7a002d24ac3dbf3e5fe44e6b3c7c
lucazx <42161414+lucazx@users.noreply.github.com>
2022-04-30 19:57:32 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ["i2.wp.com", "imgur.com", "i.imgur.com"],
   },
